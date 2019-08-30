03c3587cb4bf30032b0578e6ba20bc76ac486bac
Javier Flores <javier.flores@wolox.cl>
2019-08-30 19:38:54 -0400

    fix gemfile dev

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,6 +27,9 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
+  # Do not fallback to assets pipeline if a precompiled asset is missed.
+  config.assets.compile = false
+
   # Store uploaded files on the local file system (see config/storage.yml for options)
   config.active_storage.service = :local
 
