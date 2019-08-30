21e32d5f35264b0d431e1fdda6eb540354a96aac
Javier Flores <javier.flores@wolox.cl>
2019-08-30 19:14:51 -0400

    fix prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,5 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  Dir["#{Rails.root}/lib/*.rb"].each { |file| require file }
 end
